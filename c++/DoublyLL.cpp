41022347b9fe31e4a0a85f509bc488e657b385a3
keerthiiraj <150156729+keerthiiraj@users.noreply.github.com>
2024-10-18 23:22:31 +0530

    Update DoublyLL.cpp

--- c++/DoublyLL.cpp
+++ c++/DoublyLL.cpp
@@ -1,60 +1,64 @@
 #include <iostream>
 using namespace std;
-class Node
-{
+
+// Node class definition
+class Node {
 public:
-    Node *previous;
-    int data;
-    Node *next;
+    Node *previous; // Pointer to the previous node
+    int data;       // Data stored in the node
+    Node *next;     // Pointer to the next node
 };
-Node *Insert(int pos,int x, Node *head)
-{
-     Node *t,*p;
-     if(pos ==0)
-     {
-        t=new Node;
-        t->data=x;
-        t->previous=NULL;
-        t->next=head;
-        if(head !=NULL)
-        head->previous=t;
-        head=t;
-     }
-     else
-     {
-        p=head;
-        for(int i=0;i<pos-1;i++)
-            p=p->next;
-        t=new Node;
-        t->data=x;
-        t->previous=p;
-        t->next=p->next;
-        if(p->next)
-        p->next->previous=t;
-        p->next=t;
-     }
-     return head;
-}
-void display(Node *head)
-{
-    Node *p=head;
-    while(p!=NULL)
-    {
-        cout<<p->data<<" ";
-        p=p->next;
+
+// Function to insert a new node at a given position
+Node* Insert(int pos, int x, Node *head) {
+    Node *t = new Node; // Create a new node
+    t->data = x;        // Set the data for the new node
+
+    if (pos == 0) { // Insertion at the head
+        t->previous = NULL;
+        t->next = head;
+        if (head != NULL) {
+            head->previous = t; // Update the previous head's pointer
+        }
+        head = t; // Update head to the new node
+    } else { // Insertion at other positions
+        Node *p = head;
+        for (int i = 0; i < pos - 1 && p != NULL; i++) {
+            p = p->next; // Traverse to the position
+        }
+        if (p != NULL) {
+            t->previous = p;
+            t->next = p->next;
+            if (p->next) {
+                p->next->previous = t; // Update the next node's previous pointer
+            }
+            p->next = t; // Link the new node
+        }
     }
+    return head; // Return the updated head
 }
-int main()
-{
-    Node *first=NULL;
-    int n,x,pos;
-    cout<<"Enter the number of elements in the list: ";
-    cin>>n;
-    for(int i=0;i<n;i++)
-    {
-        cin>>x;
-        first=Insert(i,x,first);
+
+// Function to display the linked list
+void display(Node *head) {
+    Node *p = head;
+    while (p != NULL) {
+        cout << p->data << " "; // Print the data
+        p = p->next; // Move to the next node
     }
-    display(first);
-return 0;
+    cout << endl; // New line after displaying all elements
+}
+
+// Main function
+int main() {
+    Node *first = NULL; // Initialize the head of the list
+    int n, x, pos;
+    cout << "Enter the number of elements in the list: ";
+    cin >> n; // Input number of elements
+    for (int i = 0; i < n; i++) {
+        cout << "Enter element " << (i + 1) << ": ";
+        cin >> x; // Input element data
+        first = Insert(i, x, first); // Insert element into the list
+    }
+    display(first); // Display the linked list
+    return 0; // End of program
 }
